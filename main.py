84cd3ef1e788d30cc34226c5cd1a770bfb62d84f
Owen2284 <owen2284@googlemail.com>
2018-10-17 22:45:34 +0100

    Created the main file

---
+++ main.py
@@ -0,0 +1,37 @@
+def main():
+    # === INITIALISATION =======================================================================
+    playerName = input("Please enter thy name: ")
+    print("Hello " + playerName + "!")
+
+    playing = True
+    endingNumber = -1
+    
+    #playerAge = input("Please enter thy age: ")
+    #while not playerAge.isdigit():
+    #    print("Fuck off mate")
+    #    playerAge = input("Please enter thy age: ")
+    #playerAge = int(playerAge)
+
+    # === GAMELOOP =============================================================================
+    while (playing):
+        action = input("Please enter your action: ").lower()
+        print(action)
+
+        if action == "quit":
+            playing = False
+            endingNumber = 0
+    # ==========================================================================================
+
+    # === ENDINGS ==============================================================================
+    if endingNumber == 0:
+        print("You were eaten by a grue.")
+    else:
+        print("Wait what the fuck")
+
+    if endingNumber >= 0:
+        print("Thanks for playing!")
+    # ==========================================================================================
+
+# Main bootstrap method that will kick off the game.
+if __name__ == "__main__":
+    main()
