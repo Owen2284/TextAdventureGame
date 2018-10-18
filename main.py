ecd96985765973c7dfb591d0f6639e35e8d66d01
strarmvik <Draconic@live.co.uk>
2018-10-18 07:11:08 +0100

    Outline potential action menu system, spacing readability.

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 def main():
+    """
+    docstring test - main() will serve as the body of the game.
+    """
     # === INITIALISATION =======================================================================
+    print("lalalal backstory ur a skellington who wants out of this hell ayy lmao")
+    
     playerName = input("Please enter thy name: ")
     print("Hello " + playerName + "!")
 
     playing = True
+    isImmobilised = True
     endingNumber = -1
+
     
-    #playerAge = input("Please enter thy age: ")
-    #while not playerAge.isdigit():
-    #    print("Fuck off mate")
-    #    playerAge = input("Please enter thy age: ")
-    #playerAge = int(playerAge)
+    playerAge = input("Please enter thy age: ")
+    while not playerAge.isdigit():
+        print("That's not an age")
+        playerAge = input("Please enter thy age: ")
+    playerAge = int(playerAge)
+    
+
+    
     # ==========================================================================================
 
+    
     # === GAMELOOP =============================================================================
     while (playing):
+        print("What do you do?")
+        actionListPhaseOne = ["look around", "quit"]
+        print(actionListPhaseOne)
         action = input("Please enter your action: ").lower()
-        print(action)
-
+        print(">" + action)
+   
+    # actionLists will be printed at the start of each 'turn' once populated with the immediate  
+    # options available to a player. PhaseOne denotes the start of a game session.
+   
         if action == "quit":
             playing = False
             endingNumber = 0
+
+        if action == "look around" and isImmobilised is True:
+            playing = True
+            actionListPhaseOne.insert(1, "free yourself")
+            print("You try to LOOK AROUND. Sadly, your head is stuck!")
+
     # ==========================================================================================
 
+    
     # === ENDINGS ==============================================================================
     if endingNumber == 0:
         print("You were eaten by a grue.")
